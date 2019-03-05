13fdaf843c33978b0f3754e84ee0203614ef6826
CocoaZX <zx349872481@gmail.com>
2019-03-05 10:09:58 +0800

    RN视频 修改

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RNVideo
 react-native-video
+修改iOS 状态栏全屏后隐藏问题
---
+++ ios/build/Build/Intermediates.noindex/RCTVideo.build/Debug-iphonesimulator/RCTVideo.build/Objects-normal/x86_64/UIView+FindUIViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/zhangxin/Desktop/react-native-video/ios/Video/UIView+FindUIViewController.m \
+  /Users/zhangxin/Desktop/react-native-video/ios/Video/UIView+FindUIViewController.h
